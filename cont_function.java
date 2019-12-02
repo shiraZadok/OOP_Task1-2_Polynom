9be34a93cc87c643ab75324bdef56793882c8499
shiraZadok <58064644+shiraZadok@users.noreply.github.com>
2019-12-02 17:51:55 +0200

    addition of Ex1

--- cont_function.java
+++ cont_function.java
@@ -1,4 +1,4 @@
-package myMath;
+package Ex1;
 
 /**
  * The interface represents a continuance function 
@@ -8,7 +8,7 @@ package myMath;
 public interface cont_function extends function{
 	/**
 	 * Compute a Riman's integral from x0 to x1 in eps steps. 
-	 * @param x0 starting pooint
+	 * @param x0 starting point
 	 * @param x1 end point
 	 * @param eps positive step value
 	 * @return the approximated area above X-axis below this function bounded in the range of [x0,x1]
