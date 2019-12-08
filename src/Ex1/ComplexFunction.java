098baadaf7567e0d6c987542c2073154f3507a56
yiratpeleg <yiratpeleg8@gmail.com>
2019-12-08 18:57:02 +0200

    finish complexFunction

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,4 +1,6 @@
 package Ex1;
+import sun.security.krb5.internal.CredentialsUtil;
+
 import java.util.Stack;
 
 public class ComplexFunction implements complex_function {
@@ -14,17 +16,19 @@ public class ComplexFunction implements complex_function {
     public ComplexFunction(function f1){
         this.right = null;
         this.left = f1;
+
     }
 
-    public ComplexFunction(ComplexFunction n){
+    public ComplexFunction (ComplexFunction n){
         this.right = n.right;
         this.left = n.left;
         this.operator = n.operator;
     }
 
     public ComplexFunction(String op , function f1 , function f2){
-        this.right = f1;
-        this.left = f2;
+        op=op.toLowerCase();
+        this.left = f1;
+        this.right = f2;
         if(op.equals("plus")){
             this.operator = Operation.Plus;
         }
@@ -145,52 +149,97 @@ public class ComplexFunction implements complex_function {
 
     @Override
     public void comp(function f1) {
+        if(this.operator == Operation.None){
+            this.operator=Operation.Comp;
+            this.right=f1;
+        }
+        else{
+            ComplexFunction temp = new ComplexFunction();
+            temp.operator= Operation.Comp;
+            temp.left= new ComplexFunction(this);
+            temp.right=f1;
 
+            this.operator=temp.operator;
+            this.left=temp.left;
+            this.right=temp.right;
+
+        }
     }
 
     @Override
     public function left() {
-        return null;
+        return this.left;
     }
 
     @Override
     public function right() {
-        return null;
+        return this.right;
     }
 
     @Override
     public Operation getOp() {
-        return null;
+        return this.operator;
     }
 
     @Override
     public double f(double x) {
-        return 0;
+        if(this.operator==Operation.Min){
+            return (Math.min(this.left.f(x),this.right.f(x)));
+        }
+        else if(this.operator==Operation.Max){
+            return (Math.max(this.left.f(x),this.right.f(x)));
+        }
+        else if(this.operator==Operation.Divid){
+            return (this.left.f(x)/this.right.f(x));
+        }
+        else if(this.operator==Operation.Plus){
+            return (this.left.f(x)+this.right.f(x));
+        }
+        else if(this.operator==Operation.Times){
+            return (this.left.f(x)*this.right.f(x));
+        }
+        else if(this.operator==Operation.Comp){
+            return (this.left.f(this.right.f(x)));
+        }
+        else {
+            return this.left.f(x);
+        }
+    }
+
+    @Override
+    public function copy() {
+    function copyFunction = new ComplexFunction(this);
+    return copyFunction;
     }
 
     @Override
     public function initFromString(String s) {
-        String r;
-        String l="";
-        String op="";
-        int i = 0;
-        boolean b = false;
-        while (s.charAt(i)!='(' && i<s.length()){
-            op=op+s.charAt(i);
-            i++;
-            if(i==s.length()-1){
-                b=true;
-            }
-        }
-
-        int comma= this.findComma(s);
-        return null;
+        ComplexFunction p = new ComplexFunction();
+        function ans = p.initFromStringRec(s);
+        return ans ;
     }
 
+    private function initFromStringRec(String s) {
+        if (!s.contains("(")) {
+            return new Polynom(s);
+        }
+        int comma = findComma(s);
+        String op = findOp(s);
+        this.left = this.initFromStringRec(s.substring(op.length() + 1, comma));
+        this.operator = this.op(op);
+        this.right = this.initFromStringRec(s.substring(comma + 1, s.length() - 1));
+        function f = new ComplexFunction(this);
+        return f;
+    }
 
-    @Override
-    public function copy() {
-        return null;
+    public Operation op(String s1){
+        if(s1.equals("mul")) return Operation.Times;
+        else if(s1.equals("div")) return Operation.Divid;
+        else if(s1.equals("plus")) return Operation.Plus;
+        else if(s1.equals("max")) return Operation.Max;
+        else if(s1.equals("min")) return Operation.Min;
+        else if(s1.equals("comp")) return Operation.Comp;
+        return Operation.None;
     }
 
     public int findComma(String s) {
@@ -208,18 +257,42 @@ public class ComplexFunction implements complex_function {
         return temp;
     }
 
+    public String findOp(String o) {
+        String op = "";
+        int i = 0;
+        boolean b = true;
+        while (b==true && o.charAt(i) != '(') {
+            op = op + o.charAt(i);
+            if (i == o.length()-1) {
+                op = "";
+                b=false;
+            }
+            i++;
+        }
+        return op;
+    }
+
     public String toString(){
         return this.operator + "(" + this.left.toString() + "," + this.right.toString() + ")";
     }
 
     public static void main(String[] args) {
-        ComplexFunction e = new ComplexFunction("plus",new Monom(4,2),new Polynom("4x^2+1"));
-        System.out.println(e.left.toString());
-        System.out.println(e.right.toString());
-        System.out.println(e.operator);
-        e.mul(new Polynom("2x+3"));
-        System.out.println(e.toString());
-        System.out.println(e.findComma("Times(Plus(4.0x^2+1.0,4.0x^2),2.0x+3.0)"));
+        ComplexFunction e = new ComplexFunction("plus",new Monom("x"),new Monom("4"));
+//            System.out.println(e.left.toString());
+//            System.out.println(e.right.toString());
+//            System.out.println(e.operator);
+//            e.mul(new Polynom("2x+3"));
+//            System.out.println(e.toString());
+//            System.out.println(e.findComma("Times(Plus(4.0x^2+1.0,4.0x^2),2.0x+3.0)"));
+
+//        function c = e.initFromString("2.0x+3.0");
+//        ComplexFunction p = (ComplexFunctionbc;
+//        ComplexFunction f = new ComplexFunction((ComplexFunction)c);
+//        System.out.println(f.toString());
+
+//        ("div(plus(4.0x^3,4.0x^2),1.0x^2+3.0x-5.0)");
+//        function f = e.initFromString("div(plus(4.0x^3,4.0x^2),1.0x^2+3.0x-5.0)");
+//        System.out.println(f.f(2));
 
 
     }
