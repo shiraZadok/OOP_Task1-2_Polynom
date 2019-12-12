37cee45e7906842540c801a7d566757b7abe625e
yiratpeleg <yiratpeleg8@gmail.com>
2019-12-12 18:38:44 +0200

    stam Yael

--- src/Ex1/Monom.java
+++ src/Ex1/Monom.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
  * This class represents a simple "Monom" of shape a*x^b, where a is a real number and a is an integer (summed a none negative), 
  * see: https://en.wikipedia.org/wiki/Monomial 
  * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
- * @author Boaz
+ *
  *
  */
 public class Monom implements function {
